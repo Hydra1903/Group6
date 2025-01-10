50f8c8e26aa049970b6ae73b2d25c852345f7bc8
Hydra1903 <duongquochy2019@gmail.com>
2025-01-10 20:16:16 +0700

    Update

---
+++ Assets/Scripts/Demo/SaveLoadButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveLoadButton : MonoBehaviour
+{
+    private SaveLoadManager saveLoadManager;
+
+    private void Start()
+    {
+        saveLoadManager = FindObjectOfType<SaveLoadManager>();
+        // Tải game khi bắt đầu
+        saveLoadManager.LoadGame();
+    }
+
+    public void OnSaveButtonPressed()
+    {
+        saveLoadManager.SaveGame();
+    }
+
+    public void OnLoadButtonPressed()
+    {
+        saveLoadManager.LoadGame();
+    }
+}
