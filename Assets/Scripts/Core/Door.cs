b9bef091418cd6470167bb652ae127a4fd9e2c9b
Hydra1903 <duongquochy2019@gmail.com>
2025-01-13 09:58:29 +0700

    update

--- Assets/Scripts/Core/Door.cs
+++ Assets/Scripts/Core/Door.cs
@@ -1,28 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
-    [SerializeField] private string fromScene;
-    [SerializeField] private string toScene;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private string toScene; // Scene đích
+    public Vector2 targetPosition; // Vị trí nhân vật trong scene mới
+    private Animator anim; // Animator cho hiệu ứng
+    private bool isTransitioning = false; // Ngăn kích hoạt nhiều lần
+    [SerializeField] private GameObject panel;
+
+    private void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && !isTransitioning)
         {
-           SceneManager.LoadScene(toScene);
+            isTransitioning = true;
+            StartCoroutine(Transition(collision));
         }
+    }
+
+    private IEnumerator Transition(Collider2D playerStat)
+    {
+        // Bắt đầu hiệu ứng Animator
+        if (anim != null)
+        {
+            panel.SetActive(true);
+            anim.SetTrigger("NextScene");
+        }
+
+        // Đợi hiệu ứng hoàn thành
+        yield return new WaitForSeconds(1f); // Thời gian chờ theo độ dài hiệu ứng
+
+        // Đặt vị trí nhân vật
+        PlayerStat.Instance.transform.position = targetPosition;
+
+        // Chuyển scene
+        SceneManager.LoadScene(toScene);
     }
 }
---
+++ Assets/Scripts/Core/SceneData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneData : MonoBehaviour
+{
+    public static SceneData Instance;
+    public Vector2 targetPosition; // Vị trí tùy chỉnh trong scene mới.
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Giữ đối tượng này khi chuyển scene.
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
